6698cc2225edd9caac205ebcde7aa20f6b399cba
elor <elor@hrz.tu-chemnitz.de>
2013-09-18 22:10:43 +0200

    running state

--- control.sh
+++ control.sh
@@ -2,11 +2,14 @@
 #
 # This script manages music
 
-currentfile='/home/pi/current.txt'
-playlistfile='/home/pi/synced/audio/playlists.txt'
-pipe='/home/pi/code/vlcctrl/pipe.fifo'
-pidfile='/home/pi/code/vlcctrl/pid.pid'
-daemoncmd='/home/pi/code/vlcctrl/daemon.py'
+home='/home/pi'
+dir="$home/vlcctrl"
+
+playlistfile="$home/synced/audio/playlists.txt"
+currentfile="$dir/current.txt"
+pipe="$dir/pipe.fifo"
+pidfile="$dir/pid.pid"
+daemoncmd="$dir/daemon.py"
 
 get_current(){
   if [ -f "$currentfile" ];then
@@ -40,116 +43,116 @@ get_playlist(){
 }
 
 daemon_running(){
-	if [ -f "$pidfile" ];then
-		pid=`echo $(cat "$pidfile")`
+  if [ -f "$pidfile" ];then
+    pid=`echo $(cat "$pidfile")`
 
-		proc=`ps h $pid`
+    proc=`ps h $pid | grep "$daemoncmd"`
 
-		if [ -n "$proc" ]; then
-			return 0 # running
-		fi
-	fi
+    if [ -n "$proc" ]; then
+      return 0 # running
+    fi
+  fi
 
-	return 1 # not running
+  return 1 # not running
 }
 
 stop_daemon(){
-	daemon_running && kill `echo $(cat "$pidfile")`
-	sleep 1
-	if daemon_running ; then
-		echo "attempting hardkill"
-		kill -KILL `echo $(cat "$pidfile")`
-	else
-		echo "killed"
-	fi
-	rm "$pidfile"
-	rm "$pipe"
+  daemon_running && kill `echo $(cat "$pidfile")`
+  sleep 1
+  if daemon_running ; then
+    echo "attempting hardkill"
+    kill -KILL `echo $(cat "$pidfile")`
+  else
+    echo "killed"
+  fi
+  rm "$pidfile"
+  rm "$pipe"
 }
 
 start_daemon(){
-	daemon_running && stop_daemon
+  daemon_running && stop_daemon
 
-	pipecheck
+  pipecheck
 
-	$daemoncmd -p "$pipe" &
-	echo $! > "$pidfile"
+  $daemoncmd -p "$pipe" &
+  echo $! > "$pidfile"
 
-	echo "started with pid $!"
+  echo "started with pid $!"
 }
 
 pipecheck(){
-	mkfifo "$pipe" >/dev/null 2>/dev/null
+  mkfifo "$pipe" >/dev/null 2>/dev/null
 }
 
 send(){
-	echo "$1"
-	echo "$1" >> "$pipe"
+#  echo "$1"
+  echo "$1" >> "$pipe"
 }
 
 set_playlist(){
-	send "clear"
-	send "random on"
-	send "loop on"
-	send "add $1"
+  send "clear"
+  send "random on"
+  send "loop on"
+  send "add $1"
 }
 
 next(){
-	send "next"
+  send "next"
 }
 
 prev(){
-	send "prev"
+  send "prev"
 }
 
 pause(){
-	send "pause"
+  send "pause"
 }
 
 case $1 in
-	right)
-		daemon_running || echo "daemon not running" >&2
-		next
-		;;
-	left)
-		daemon_running || echo "daemon not running" >&2
-		prev
-		;;
-	center)
-		daemon_running || echo "daemon not running" >&2
-		pause
-		;;
+  next)
+#    daemon_running || echo "daemon not running" >&2
+    next
+    ;;
+  prev)
+#    daemon_running || echo "daemon not running" >&2
+    prev
+    ;;
+  pause)
+#    daemon_running || echo "daemon not running" >&2
+    pause
+    ;;
   up)
-		daemon_running || echo "daemon not running" >&2
+#    daemon_running || echo "daemon not running" >&2
     line=`get_current`
     let line++
     crop_current $line
     save_current $line
-		set_playlist "$(get_playlist $(get_current))"
+    set_playlist "$(get_playlist $(get_current))"
     ;;
   down)
-		daemon_running || echo "daemon not running" >&2
+#    daemon_running || echo "daemon not running" >&2
     line=`get_current`
     let line--
     crop_current $line
     save_current $line
-		set_playlist "$(get_playlist $(get_current))"
+    set_playlist "$(get_playlist $(get_current))"
     ;;
   start)
-		start_daemon
-		set_playlist "$(get_playlist $(get_current))"
+    start_daemon
+    set_playlist "$(get_playlist $(get_current))"
     ;;
   stop)
-		stop_daemon
+    stop_daemon
     ;;
-	status)
-		if daemon_running ; then
-			echo "daemon running with pid `echo $(cat "$pidfile")`"
-		else
-			echo "daemon not running"
-		fi
-		;;
+  status)
+    if daemon_running ; then
+      echo "daemon running with pid `echo $(cat "$pidfile")`"
+    else
+      echo "daemon not running"
+    fi
+    ;;
   *)
-    echo "usage: $0 left|right|up|down|center|start|stop|status"
+    echo "usage: $0 prev|next|up|down|pause|start|stop|status"
     exit 1
     ;;
 esac
